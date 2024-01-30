bbab5bc1d2082588d2a179bf9da1e5381a7b8360
anuprshetty <anuprshetty@gmail.com>
2024-01-30 20:33:31 +0530

    nft_bridge smart contract setup

---
+++ contracts/nft_bridge.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTBridge is IERC721Receiver, ReentrancyGuard, Ownable {
+    IERC721Enumerable public nftMinter;
+    IERC20 public feeToken;
+    uint256 public nftMovingFeeNative = 0.0025 ether;
+    uint256 public nftMovingFeeCustom = 10;
+}
