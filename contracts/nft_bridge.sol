2ad48bf3fe226639a21f38e1792966cf7dbac2f9
anuprshetty <anuprshetty@gmail.com>
2024-02-01 07:13:50 +0530

    custodialNFTs

--- contracts/nft_bridge.sol
+++ contracts/nft_bridge.sol
@@ -13,4 +13,11 @@ contract NFTBridge is IERC721Receiver, ReentrancyGuard, Ownable {
     IERC20 public feeToken;
     uint256 public nftMovingFeeNative = 0.0025 ether;
     uint256 public nftMovingFeeCustom = 10;
+
+    struct CustodialNFT {
+        uint256 tokenId;
+        address holder;
+    }
+
+    mapping(uint256 tokenId => CustodialNFT custodialNFT) public custodialNFTs;
 }
